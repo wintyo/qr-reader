8ddffeb4f8d3175dee7bcd202a3885c34cf12f59
wintyo <wintyo1024@gmail.com>
2021-04-04 18:15:52 +0900

    Vue3でpug + sass環境を用意する

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+div
+  p.test__hoge test
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+})
+</script>
+
+<style lang="scss" scoped>
+.test {
+  color: red;
+
+  &__hoge {
+    font-size: 18px;
+    font-weight: bold;
+  }
+}
+</style>
