0e1218ec849ef65d73d9b4ad4416533bd53eb574
wintyo <wintyo1024@gmail.com>
2021-04-05 08:52:56 +0900

    PWAの設定をする

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import { registerSW } from 'virtual:pwa-register';
+
+registerSW();
+
 createApp(App).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    VitePWA({
+      manifest: {
+        name: 'QR Reader',
+        short_name: 'QR',
+        background_color: '#fff',
+        theme_color: '#3cb371',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: 'app-icons/512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
